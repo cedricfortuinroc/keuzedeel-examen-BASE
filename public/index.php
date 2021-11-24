41b0923f135d57457d22b8c96743e6b7c055ad00
cedricfortuinroc <78347141+cedricfortuinroc@users.noreply.github.com>
2021-11-24 19:41:47 +0100

    update

--- public/index.php
+++ public/index.php
@@ -9,7 +9,8 @@
 
     <link rel="stylesheet" href="assets/css/app.css">
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" 
+    crossorigin="anonymous"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body>
